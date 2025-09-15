b3ea1fedbfd14350b8dcc27c6c40fdbb64502e53
Gustavo Semenuk <gustavo.semenuk638@hotmail.com>
2025-09-15 00:34:41 -0300

    v26

--- Hermes.ai/ms_clusterizacao/services/databricks_services.py
+++ Hermes.ai/ms_clusterizacao/services/databricks_services.py
@@ -36,9 +36,8 @@ class DatabricksService:
 
 class ClusterDataService:
     def __init__(self):
-        self.catalog_path = os.path.join(
-            os.path.dirname(__file__), "Hermes.ai/ms_clusterizacao/arquivo_parquet/cluster_catalog.parquet"
-        )
+        self.catalog_path = os.path.join(base_dir, "..", "cluster_catalog.parquet")
+
 
     def get_catalog(self) -> pd.DataFrame:
         return pd.read_parquet(self.catalog_path)
